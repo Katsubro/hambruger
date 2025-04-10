3df2208a23f0caa34b4410e826cfe67f11388861
Katsubro <Kiyoshi.813843@nv.ccsd.net>
2025-04-10 07:54:09 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 
+// Toggle menu for mobile view
+function toggleMenu() {
+  const menu = document.getElementById('menu');
+  const hamburger = document.querySelector('.hamburger');
+  
+  menu.classList.toggle('show');
+  
+  // Toggle animation for hamburger icon
+  hamburger.classList.toggle('open');
+}
